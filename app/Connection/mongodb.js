0224639621a81bd6f1ea5a0a91e94bec588cc6d5
Mayank <chand.mayank21@gmail.com>
2022-11-24 18:13:46 +0530

    resolved merge conflict

--- app/Connection/mongodb.js
+++ app/Connection/mongodb.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
-const mongoUsername=process.env.DB_USER;
-const mongoPassword=process.env.DB_PASS;
-const mongoURI=`mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.qn9faxx.mongodb.net/?retryWrites=true&w=majority`;
+const mongoUsername = process.env.DB_USER;
+const mongoPassword = process.env.DB_PASS;
+
+
+const mongoURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.qn9faxx.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }).
-then(()=>console.log("connected")).catch(err=>console.log(err));
+    then(() => console.log("connected")).catch(err => console.log(err));
