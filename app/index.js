115d626dca07f1a7583c2c4ad3a467aa046ef1ca
HarshaK47 <harsha49code@gmail.com>
2022-11-25 00:07:39 +0530

    fetch file names

--- app/index.js
+++ app/index.js
@@ -6,21 +6,18 @@ import "./Connection/mongodb.js";// must be called before others
 //routes
 // import code_generator from './routes/code_generator.js';
 import json_transformer from './routes/json_transformer.js';
+import file_names from './routes/file_names.js';
+
 import env from 'dotenv';
 
-
 env.config();
-
-
-
-
-
 const app = express();
 
 //middlewares
 app.use(express.json());
 // app.use('/generate',code_generator);
 app.use('/transform', json_transformer);
+app.use('/file_names', file_names);
 
 app.listen(3000, () => {
     console.log('Server is running at 3000');
