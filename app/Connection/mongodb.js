1aca030c8e684b1d6a17c0e0a3cf59ad97b08ad7
Mayank <chand.mayank21@gmail.com>
2022-11-24 19:20:15 +0530

    resolved db issue

--- app/Connection/mongodb.js
+++ app/Connection/mongodb.js
@@ -3,7 +3,12 @@ const mongoUsername = process.env.DB_USER;
 const mongoPassword = process.env.DB_PASS;
 
 
-const mongoURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.qn9faxx.mongodb.net/?retryWrites=true&w=majority`;
+const mongoURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.qn9faxx.mongodb.net/?retryWrites=true&w=majority&ssl=true`;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }).
-    then(() => console.log("connected")).catch(err => console.log(err));
+mongoose.connect(
+    mongoURI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("connected")).catch(err => console.log(err));
--- app/index.js
+++ app/index.js
@@ -1,10 +1,28 @@
+// import "./Connection/mongodb.js";// must be called before others 
 import express from "express";
-import  "./Connection/mongodb.js";// must be called before others 
-import {conn,upload,gfs,gridFSBucket} from "./Connection/multer_GridFS.js";
-
-//routes
+import mongoose from 'mongoose'
 import code_generator from './routes/code_generator.js';
 import json_transformer from './routes/json_transformer.js';
+import env from 'dotenv';
+
+
+env.config();
+
+
+// mongodb connection
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qn9faxx.mongodb.net/?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        console.log("Database Connected Successfully");
+    });
+
+
 
 const app = express();
 
